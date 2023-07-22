8506642dbbf01fc4bfc75b1630d39df9ef84a3b3
imelshety <mohamedkingman99@gmail.com>
2023-07-22 18:16:48 +0300

    Update OurServices Component Layout

--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -10,56 +10,54 @@ const OurServices = () => {
                 <p className="w-3/4 text-slate-500 text-sm leading-7 py-4 lg:max-w-2xl">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti
                     atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,</p>
             </div>
-            <div className="container mt-8 flex flex-col items-start justify-center lg:flex-row lg:items-center">
-                <div>
-                    <div className="w-full pl-32 py-8 leading-8 relative lg:w-1/4 lg:pl-13 lg:text-right">
-                        <div className="w-20 h-20  bg-[#ffeafc] rounded-full flex justify-center items-center absolute left-3">
-                            <img src="/img/icon4.png" alt="icon-1" />
-                        </div>
-
-                        <h3 className="text-2xl font-semibold ">Web Design</h3>
-                        <p className="text-slate-500 text-sm py-3">At vero eos et accusamus et iusto odio digniss imos ducimus qui bla nditiis pro esentium volu ptatum deleniti atque pecorrupti.</p>
+            <div className="container my-8 flex flex-col items-start justify-center lg:w-[80%] lg:mx-auto lg:flex-row lg:items-center lg:flex-wrap">
+                <div className="w-full pl-32 py-8 leading-8 relative lg:w-1/3 lg:pl-13">
+                    <div className="w-20 h-20  bg-[#ffeafc] rounded-full flex justify-center items-center absolute left-3">
+                        <img src="/img/icon4.png" alt="icon-1" />
                     </div>
-                    <div className="w-full pl-32 py-4 leading-8 relative lg:w-1/4 lg:pl-13 lg:text-right">
-                        <div className="w-20 h-20  bg-[#ffeafc] rounded-full flex justify-center items-center absolute left-3">
-                            <img src="/img/icon5.png" alt="icon-5" />
-                        </div>
 
-                        <h3 className="text-2xl font-semibold ">Web Design</h3>
-                        <p className="text-slate-500 text-sm py-3">At vero eos et accusamus et iusto odio digniss imos ducimus qui bla nditiis pro esentium volu ptatum deleniti atque pecorrupti.</p>
+                    <h3 className="text-2xl font-semibold ">Web Design</h3>
+                    <p className="text-slate-500 text-sm py-3">At vero eos et accusamus et iusto odio digniss imos ducimus qui bla nditiis pro esentium volu ptatum deleniti atque pecorrupti.</p>
+                </div>
+                <div className="w-full pl-32 py-4 leading-8 relative lg:w-1/3 lg:pl-13">
+                    <div className="w-20 h-20  bg-[#ffeafc] rounded-full flex justify-center items-center absolute left-3">
+                        <img src="/img/icon5.png" alt="icon-5" />
                     </div>
-                    <div className="w-full pl-32 py-8 leading-8 relative lg:w-1/4 lg:pl-13 lg:text-right">
-                        <div className="w-20 h-20  bg-[#ffeafc] rounded-full flex justify-center items-center absolute left-3">
-                            <img src="/img/icon6.png" alt="icon-6" />
-                        </div>
 
-                        <h3 className="text-2xl font-semibold ">Web Design</h3>
-                        <p className="text-slate-500 text-sm py-3">At vero eos et accusamus et iusto odio digniss imos ducimus qui bla nditiis pro esentium volu ptatum deleniti atque pecorrupti.</p>
+                    <h3 className="text-2xl font-semibold ">Web Design</h3>
+                    <p className="text-slate-500 text-sm py-3">At vero eos et accusamus et iusto odio digniss imos ducimus qui bla nditiis pro esentium volu ptatum deleniti atque pecorrupti.</p>
+                </div>
+                <div className="w-full pl-32 py-8 leading-8 relative lg:w-1/3 lg:pl-13">
+                    <div className="w-20 h-20  bg-[#ffeafc] rounded-full flex justify-center items-center absolute left-3">
+                        <img src="/img/icon6.png" alt="icon-6" />
                     </div>
-                    <div className="w-full pl-32 py-8 leading-8 relative lg:w-1/4 lg:pl-13 lg:text-right">
-                        <div className="w-20 h-20  bg-[#ffeafc] rounded-full flex justify-center items-center absolute left-3">
-                            <img src="/img/icon7.png" alt="icon-7" />
-                        </div>
 
-                        <h3 className="text-2xl font-semibold ">Web Design</h3>
-                        <p className="text-slate-500 text-sm py-3">At vero eos et accusamus et iusto odio digniss imos ducimus qui bla nditiis pro esentium volu ptatum deleniti atque pecorrupti.</p>
+                    <h3 className="text-2xl font-semibold ">Web Design</h3>
+                    <p className="text-slate-500 text-sm py-3">At vero eos et accusamus et iusto odio digniss imos ducimus qui bla nditiis pro esentium volu ptatum deleniti atque pecorrupti.</p>
+                </div>
+                <div className="w-full pl-32 py-8 leading-8 relative lg:w-1/3 lg:pl-13">
+                    <div className="w-20 h-20  bg-[#ffeafc] rounded-full flex justify-center items-center absolute left-3">
+                        <img src="/img/icon7.png" alt="icon-7" />
                     </div>
-                    <div className="w-full pl-32 py-8 leading-8 relative lg:w-1/4 lg:pl-13 lg:text-right">
-                        <div className="w-20 h-20  bg-[#ffeafc] rounded-full flex justify-center items-center absolute left-3">
-                            <img src="/img/icon8.png" alt="icon-8" />
-                        </div>
 
-                        <h3 className="text-2xl font-semibold ">Web Design</h3>
-                        <p className="text-slate-500 text-sm py-3">At vero eos et accusamus et iusto odio digniss imos ducimus qui bla nditiis pro esentium volu ptatum deleniti atque pecorrupti.</p>
+                    <h3 className="text-2xl font-semibold ">Web Design</h3>
+                    <p className="text-slate-500 text-sm py-3">At vero eos et accusamus et iusto odio digniss imos ducimus qui bla nditiis pro esentium volu ptatum deleniti atque pecorrupti.</p>
+                </div>
+                <div className="w-full pl-32 py-8 leading-8 relative lg:w-1/3 lg:pl-13">
+                    <div className="w-20 h-20  bg-[#ffeafc] rounded-full flex justify-center items-center absolute left-3">
+                        <img src="/img/icon8.png" alt="icon-8" />
                     </div>
-                    <div className="w-full pl-32 py-8 mb-5 leading-8 relative lg:w-1/4 lg:pl-13 lg:text-right">
-                        <div className="w-20 h-20  bg-[#ffeafc] rounded-full flex justify-center items-center absolute left-3">
-                            <img src="/img/icon9.png" alt="icon-9" />
-                        </div>
 
-                        <h3 className="text-2xl font-semibold ">Web Design</h3>
-                        <p className="text-slate-500 text-sm py-3">At vero eos et accusamus et iusto odio digniss imos ducimus qui bla nditiis pro esentium volu ptatum deleniti atque pecorrupti.</p>
+                    <h3 className="text-2xl font-semibold ">Web Design</h3>
+                    <p className="text-slate-500 text-sm py-3">At vero eos et accusamus et iusto odio digniss imos ducimus qui bla nditiis pro esentium volu ptatum deleniti atque pecorrupti.</p>
+                </div>
+                <div className="w-full pl-32 py-8  leading-8 relative lg:w-1/3 lg:pl-13">
+                    <div className="w-20 h-20  bg-[#ffeafc] rounded-full flex justify-center items-center absolute left-3">
+                        <img src="/img/icon9.png" alt="icon-9" />
                     </div>
+
+                    <h3 className="text-2xl font-semibold ">Web Design</h3>
+                    <p className="text-slate-500 text-sm py-3">At vero eos et accusamus et iusto odio digniss imos ducimus qui bla nditiis pro esentium volu ptatum deleniti atque pecorrupti.</p>
                 </div>
             </div>
         </section>
